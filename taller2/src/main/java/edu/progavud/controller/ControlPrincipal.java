b46ae6862dad71841b4a9e36706b0d7b5ef56cdc
N0dadux <juddiazp@udistrital.edu.co>
2025-04-30 18:21:32 +0000

    Update: Eliminacion de errores en el programa. Add: Metodo colocarApuestas.

--- taller2/src/main/java/edu/progavud/controller/ControlPrincipal.java
+++ taller2/src/main/java/edu/progavud/controller/ControlPrincipal.java
@@ -12,13 +12,9 @@ package edu.progavud.controller;
  */
 public class ControlPrincipal {
     /**
-     * Objeto que representa el control del crupier.
+     * Objeto que representa el control del crupier y los jugadores.
      */
-    private ControlPersona controlCrupier;
-    /**
-     * Objeto que representa el control de los jugadores.
-     */
-    private ControlPersona controlJugador;
+    private ControlPersona controlPersona;
     /**
      * Objeto que representa el control de la vista.
      */
@@ -28,8 +24,65 @@ public class ControlPrincipal {
      * Método constructor que enlaza la comunicación con todos los controles.
      */
     public ControlPrincipal(){
-        controlCrupier = new ControlPersona(this,0);
-        controlJugador = new ControlPersona(this, 1);
+        controlPersona = new ControlPersona(this);
         controlVista = new ControlVista(this);
     }
+
+        /**
+     * Método heredado de la clase Persona, dobla el valor de la
+     * apuesta pero solo recibe una carta
+     */
+    public void doblar() {
+
+    }
+
+    /**
+     * Método heredado de la clase Persona,si tiene el mismo
+     * valor en las dos cartas primeras, puede dividir sus cartas
+     * en dos manos y generar otra apuesta con la nueva mano
+     */
+    public void dividir() {
+
+    }
+
+    /**
+     * Método heredado de la clase Persona, puede asegurar
+     * apostando a que le salga blackjack al crupier,
+     * si su primera carta es un as. se paga 2 a 1.
+     * Se puede apostar como maximo, la mitad de lo que aposto
+     * en la apuesta inicial.
+     */
+    public void asegurar() {
+
+    }
+
+    /**
+     * Método heredado de la clase Persona, se paga
+     * 3 a 2, si en las primeras dos cartas le sale blackjack,
+     * es decir que suman 21.
+     */
+    public void blackjack() {
+
+    }
+
+    /**
+     * Método heredado de la clase Persona, el jugador se
+     * planta cuando crea que no necesita mas cartas.
+     */
+    public void plantarse() {
+
+    }
+
+
+    /**
+     * Método que retorna el ganador de cada ronda
+     * 
+     * @return cadena con el nombre del ganador de la ronda
+     */
+    public String hallarGanador() {
+        String ganador;
+        ganador = "";
+        return ganador;
+    }
+
 }
