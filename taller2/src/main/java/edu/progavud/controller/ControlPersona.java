8b4565ee947f182ae3154b1e33006ef4e2564edf
N0dadux <juddiazp@udistrital.edu.co>
2025-04-30 17:48:28 +0000

    Update: avance en la logica del programa, cambio en las relaciones de ControlMazo, ControlJuego y ControlPersona

--- taller2/src/main/java/edu/progavud/controller/ControlPersona.java
+++ taller2/src/main/java/edu/progavud/controller/ControlPersona.java
@@ -15,40 +15,89 @@ import edu.progavud.model.Persona;
  */
 public class ControlPersona {
     /**
-     * Objeto de clase Persona, el cual será controlado por esta clase.
-     */
-    private Persona persona;
-    /**
-     * Objeto de clase ControlPrincipal, para mantener la comunicación con este objeto.
+     * Objeto de clase ControlPrincipal, para mantener la comunicación con este
+     * objeto.
      */
     private ControlPrincipal controlPrincipal;
-    
-    /**
-     * Método constructor para crear un control de Personas.
-     * Si el entero tipo persona es 0, significa que la persona es 
-     * crupier; si es 1, significa que es jugador.
-     * Usa sustitución de Liskov.
-     * 
-     * @param tipoPersona 0 o 1, 0 para crupier y 1 para jugador.
-     * @param controlPrincipal control principal para la comunicación
-     */
-    public ControlPersona(ControlPrincipal controlPrincipal, int tipoPersona){
-        if (tipoPersona == 0){
-            persona = new Crupier();
-        }else{
-            persona = new Jugador();
-        }
+    private Persona[] mesa;
+    private static int contador;
+
+    public ControlPersona(ControlPrincipal controlPrincipal) {
         this.controlPrincipal = controlPrincipal;
-    } 
-    
+    }
+
+    public void crearMesa() {
+        mesa = new Persona[3]; 
+        for (int i = 1; i < mesa.length; i++) {
+            if (i == 1) {
+                mesa[i] = new Crupier();
+            } else {
+                mesa[i] = new Jugador();
+            }
+        }
+    }
+
+
+    public void colocarApuestas(int apuesta ) {
+        contador = 0;
+        if(contador%2 == 0 && ){
+        }
+    }
+
+    /**
+     * Método heredado de la clase Persona, dobla el valor de la
+     * apuesta pero solo recibe una carta
+     */
+    public void doblar() {
+
+    }
+
+    /**
+     * Método heredado de la clase Persona,si tiene el mismo
+     * valor en las dos cartas primeras, puede dividir sus cartas
+     * en dos manos y generar otra apuesta con la nueva mano
+     */
+    public void dividir() {
+
+    }
+
+    /**
+     * Método heredado de la clase Persona, puede asegurar
+     * apostando a que le salga blackjack al crupier,
+     * si su primera carta es un as. se paga 2 a 1.
+     * Se puede apostar como maximo, la mitad de lo que aposto
+     * en la apuesta inicial.
+     */
+    public void asegurar() {
+
+    }
+
+    /**
+     * Método heredado de la clase Persona, se paga
+     * 3 a 2, si en las primeras dos cartas le sale blackjack,
+     * es decir que suman 21.
+     */
+    public void blackjack() {
+
+    }
+
+    /**
+     * Método heredado de la clase Persona, el jugador se
+     * planta cuando crea que no necesita mas cartas.
+     */
+    public void plantarse() {
+
+    }
+
     /**
      * Método que retorna el ganador de cada ronda
+     * 
      * @return cadena con el nombre del ganador de la ronda
      */
-    public String hallarGanador(){
+    public String hallarGanador() {
         String ganador;
         ganador = "";
         return ganador;
     }
-}
 
+}
--- taller2/src/main/java/edu/progavud/model/Persona.java
+++ taller2/src/main/java/edu/progavud/model/Persona.java
@@ -31,46 +31,50 @@ public abstract class Persona {
      * Número entero que representa el dinero que tiene cada persona
      */
     private int dinero;
-
+    /**
+     * Arreglo de cartas que posee la persona durante el juego.
+     */
     private ArrayList<Carta> Mano;
+
     /**
      * Método constructor de la clase que solamente inicializa los atributos
      */
-    public Persona(){
+    public Persona() {
         nombre = "";
         apellido = "";
         cedula = "";
         dinero = 0;
     }
-    
+
     /**
-    * Método constructor con los valores proporcionados.
-    * 
-    * @param nombre El nombre de la persona.
-    * @param apellido El apellido de la persona.
-    * @param cedula La cédula de identidad de la persona.
-    * @param dinero El dinero disponible de la persona.
-    * @param carta El valor de la carta asignado a una posicion del vector cartas.
-    * @param numCarta La posición del vector de cartas,
-    * es decir en que orden le salió la carta a la persona.
-    */
-    public Persona(String nombre, String apellido, String cedula, int dinero){
+     * Método constructor con los valores proporcionados.
+     * 
+     * @param nombre   El nombre de la persona.
+     * @param apellido El apellido de la persona.
+     * @param cedula   La cédula de identidad de la persona.
+     * @param dinero   El dinero disponible de la persona.
+     * @param carta    El valor de la carta asignado a una posicion del vector
+     *                 cartas.
+     * @param numCarta La posición del vector de cartas,
+     *                 es decir en que orden le salió la carta a la persona.
+     */
+    public Persona(String nombre, String apellido, String cedula, int dinero) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.cedula = cedula;
         this.dinero = dinero;
 
     }
-    
+
     /**
      * Método abstracto que calcula el dinero de la persona despues de un movimiento
      * que llamaremos transacción (apuesta, doblar, dividir, etc).
      * Se heredará en clase Jugador y Crupier.
      * 
-     * @param transaccion monto de la transaccion para sumarse al dinero                
+     * @param transaccion monto de la transaccion para sumarse al dinero
      */
     public abstract void calcularDinero(int transaccion);
-    
+
     /**
      * Método abstracto para que los jugadores y el crupier pidan carta.
      * Se heredará en clase Persona y Crupier.
@@ -78,6 +82,28 @@ public abstract class Persona {
      * @return valor de la carta que recibió
      */
     public abstract int pedirCarta();
-   
-    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public ArrayList<Carta> getMano() {
+        return Mano;
+    }
+
+    public int getDinero() {
+        return dinero;
+    }
+
+    public void setDinero(int dinero) {
+        this.dinero = dinero;
+    }
 }
